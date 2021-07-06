0a112edfc2ab3a62b346f3ae4187b960d22e555c
Reagan Charana <rgncharana2@gmail.com>
2021-07-06 22:54:16 +0300

    added get and delete endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const cors = require('cors');
-const firebaseHelper = require('firebase-functions-helper')
+const admin = require("firebase-admin");
+const cors = require("cors");
+const firebaseHelper = require("firebase-functions-helper");
 
 admin.initializeApp();
 const app = express();
 const db = admin.firestore();
-
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -21,46 +20,76 @@ const db = admin.firestore();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// CREATE ITEM
 
-app.post('/create', async (req, res) => {
-    const item = {
-        name: req.body.name,
-        description: req.body.description,
-        unitPrice: req.body.unitPrice,
-        quantity: req.body.quantity
-    }
+app.post("/create", async (req, res) => {
+  const item = {
+    name: req.body.name,
+    description: req.body.description,
+    unitPrice: req.body.unitPrice,
+    quantity: req.body.quantity,
+  };
 
-    const inventory_item = await db.collection('inventory').add(item);
-    let inventoryDocument = db.collection('inventory').doc(inventory_item.id)
-    const doc = await inventoryDocument.get();
+  const inventory_item = await db.collection("inventory").add(item);
+  let inventoryDocument = db.collection("inventory").doc(inventory_item.id);
+  const doc = await inventoryDocument.get();
 
-    if(!doc.exists){
-        res.status(500).send("Internal Server Error")
-    }else {
-        res.json({ID: doc.id, Item : doc.data(), Message: "Successfully added to inventory"})
-    }
-})
+  if (!doc.exists) {
+    res.status(500).send("Internal Server Error");
+  } else {
+    res
+      .status(200)
+      .json({
+        ID: doc.id,
+        Item: doc.data(),
+        Message: "Successfully added to inventory",
+      });
+  }
+});
 
-app.put('/update/:itemId', async (req, res) => {
+// UPDATE ITEM
 
-    let inventoryDocument = db.collection('inventory').doc(req.params.itemId)
+app.put("/update/:itemId", async (req, res) => {
+  let inventoryDocument = db.collection("inventory").doc(req.params.itemId);
 
-    const updated_item = await inventoryDocument.update(req.body)
-    
-    //const updated_item = await firebaseHelper.firestore.updateDocument(db, 'inventory', req.params.itemId, req.body);
+  const updated_item = await inventoryDocument.update(req.body);
 
-    const doc = await inventoryDocument.get();
+  const doc = await inventoryDocument.get();
 
-    if(!doc.exists){
-        res.status(500).send("Internal Server Error")
-    }else {
-        res.json({ID: doc.id, Item : doc.data(), Message: "Update Successful"})
-    }
+  if (!doc.exists) {
+    res.status(500).send("Internal Server Error");
+  } else {
+    res
+      .status(200)
+      .json({ ID: doc.id, Item: doc.data(), Message: "Update Successful" });
+  }
+});
 
-    
-})
+// VIEW/GET ITEMS
+app.get("/get", async (req, res) => {
+  let inventoryCollection = db.collection("inventory");
 
+  await inventoryCollection
+    .get()
+    .then((snapshot) => {
+      const allDocuments = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      res.status(200).send(allDocuments);
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
+// DELETE AN ITEM
 
+app.delete("/delete/:itemId", async (req, res) => {
+  let inventoryDocument = db.collection("inventory").doc(req.params.itemId);
 
-exports.inventoryAPI = functions.https.onRequest(app)
+  await inventoryDocument.delete();
+
+  res
+    .status(200)
+    .json({ ID: req.params.itemId, Message: "Deleted Successfully" });
+});
+
+exports.inventoryAPI = functions.https.onRequest(app);

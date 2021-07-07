9718fc048d387c34f3240387d2a36400d5f20b71
Reagan Charana <rgncharana2@gmail.com>
2021-07-07 10:19:53 +0300

    code cleanup

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors");
-const firebaseHelper = require("firebase-functions-helper");
 
 admin.initializeApp();
 const app = express();
